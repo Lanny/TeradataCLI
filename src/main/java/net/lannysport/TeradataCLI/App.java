96a719ed57c7cc1114c22c0089797f2f80b1f29a
RyanJenkins <Ryan.Jenkins@wellsfargo.com>
2016-02-05 12:23:49 -0800

    Started mvn project.

---
+++ src/main/java/net/lannysport/TeradataCLI/App.java
@@ -0,0 +1,13 @@
+package net.lannysport.TeradataCLI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
